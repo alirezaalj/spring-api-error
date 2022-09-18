46f0d885bbda056759ae5f2f4973583c90638db9
alireza <alirezaalijani.ir@gmail.com>
2022-09-19 00:15:47 +0430

    Initial commit

---
+++ src/main/java/ir/alirezaalijani/spring/api/error/SpringApiErrorApplication.java
@@ -0,0 +1,13 @@
+package ir.alirezaalijani.spring.api.error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiErrorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiErrorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/alirezaalijani/spring/api/error/SpringApiErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.alirezaalijani.spring.api.error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiErrorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
